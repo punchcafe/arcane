1716ec94807aa2776e3c78b22fc2554b4c0b1d3f
Luca Fruzza <lfruzza@hotmail.co.uk>
2020-02-10 22:05:48 +0000

    :construction: add foundation for class finder

--- src/main/java/dev/punchcafe/arcane/instantiate/AnnotatedClassesSeeker.java
+++ src/main/java/dev/punchcafe/arcane/instantiate/AnnotatedClassesSeeker.java
@@ -9,9 +9,7 @@ import java.io.IOException;
 import java.lang.annotation.Annotation;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -19,14 +17,14 @@ import java.util.stream.Collectors;
  */
 public class AnnotatedClassesSeeker {
 
-    public static List<Class<?>> allSpellBookPages(String path) throws IOException {
+    public static List<Class<?>> allSpellBookPages(String classpath) throws IOException {
         //TODO: ensure this uses an actual classpath
-        return allSpellBookPageClasses(classFileFinder(path));
+        List<String> paths = List.of(classpath.split(":"));
+        return allSpellBookPageClasses(classFileFinder(classpath));
     }
 
     public static List<Class<?>> allSpellBookPageClasses(List<String> classNames){
         List<Class<?>> classes = new ArrayList<>();
-
         for (String className : classNames) {
             try {
                 Class<?> clazz = Class.forName(className);
@@ -53,6 +51,7 @@ public class AnnotatedClassesSeeker {
          */
         List<String> classes = new ArrayList<>();
         Set<Path> paths = Files.list(new File(path).toPath()).collect(Collectors.toSet());
+        Map<String,List<String>> classDefinitions = new HashMap<>();
         for (Path p : paths) {
             if ((new File(p.toString())).isDirectory()) {
                 classes.addAll(classFileFinder(p.toString()));
@@ -60,8 +59,15 @@ public class AnnotatedClassesSeeker {
                 if (p.toString().endsWith(".java")) {
                     classes.add(getFullClassName(p.toString()));
                 }
+                if(p.toString().endsWith(".class")) {
+                    List<String> appended = Optional.ofNullable(classDefinitions.get(p.getFileName().toString())).orElse(new ArrayList<>());
+                    appended.add(p.toString());
+                    classDefinitions.put(p.getFileName().toString(), appended);
+                }
             }
         }
+        // TODO: use the class definitions list with l-1 index and work backwards when one.
+        // TODO: when multiple, work until the amount of loaded classes is equal to the list.
         return classes;
     }
 
