6495ccf28302b6d82a2524f1a7d76d6439229a6b
Luca Fruzza <lfruzza@hotmail.co.uk>
2020-02-11 20:42:44 +0000

    :recycle: load based on class files as opposed to java files

--- src/main/java/dev/punchcafe/arcane/instantiate/AnnotatedClassesSeeker.java
+++ src/main/java/dev/punchcafe/arcane/instantiate/AnnotatedClassesSeeker.java
@@ -20,71 +20,73 @@ public class AnnotatedClassesSeeker {
     public static List<Class<?>> allSpellBookPages(String classpath) throws IOException {
         //TODO: ensure this uses an actual classpath
         List<String> paths = List.of(classpath.split(":"));
-        return allSpellBookPageClasses(classFileFinder(classpath));
+        Map<String, List<String>> classMap = classFileFinder(classpath, new HashMap<>());
+        List<Class<?>> allDiscoveredClasses = loadClasses(classMap);
+
+        return allSpellBookPageClasses(allDiscoveredClasses);
     }
 
-    public static List<Class<?>> allSpellBookPageClasses(List<String> classNames){
+    public static List<Class<?>> allSpellBookPageClasses(List<Class<?>> classNames){
         List<Class<?>> classes = new ArrayList<>();
-        for (String className : classNames) {
-            try {
-                Class<?> clazz = Class.forName(className);
+        for (Class<?> clazz : classNames) {
                 for(Annotation annotation : clazz.getAnnotations()){
                     if (annotation.annotationType() == SpellBookPage.class){
                         classes.add(clazz);
                     }
                 }
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            }
         }
         return classes;
     }
 
 
-    private static List<String> classFileFinder(String path) throws IOException {
-        /**
-         * Instead start with the .class path and expand/contract depending on file
-         * names until the class has been successfully loaded.
-         * Alternatively, attempt to use recursion to find the package. Or at least
-         * always start at the file above, and work outwards. If it's none of those then it's the default
-         * package.
-         */
-        List<String> classes = new ArrayList<>();
+    private static Map<String,List<String>> classFileFinder(String path, Map<String,List<String>> map) throws IOException {
         Set<Path> paths = Files.list(new File(path).toPath()).collect(Collectors.toSet());
-        Map<String,List<String>> classDefinitions = new HashMap<>();
         for (Path p : paths) {
             if ((new File(p.toString())).isDirectory()) {
-                classes.addAll(classFileFinder(p.toString()));
+                classFileFinder(p.toString(), map);
             } else {
-                if (p.toString().endsWith(".java")) {
-                    classes.add(getFullClassName(p.toString()));
-                }
                 if(p.toString().endsWith(".class")) {
-                    List<String> appended = Optional.ofNullable(classDefinitions.get(p.getFileName().toString())).orElse(new ArrayList<>());
+                    List<String> appended = Optional.ofNullable(map.get(removeClassExtension(p.getFileName().toString()))).orElse(new ArrayList<>());
                     appended.add(p.toString());
-                    classDefinitions.put(p.getFileName().toString(), appended);
+                    map.put(removeClassExtension(p.getFileName().toString()), appended);
                 }
             }
         }
-        // TODO: use the class definitions list with l-1 index and work backwards when one.
-        // TODO: when multiple, work until the amount of loaded classes is equal to the list.
-        return classes;
+
+        // TODO: enable loading of Default Package classes
+        // TODO: extract .class extensions to use static constants to determine length
+        return map;
     }
 
-    private static String getFullClassName(String javaFilePath) throws IOException {
-        File file = new File(javaFilePath);
+    private static String removeClassExtension(String classFileName){
+        return classFileName.substring(0, classFileName.length()-6);
+    }
 
-        BufferedReader br = new BufferedReader(new FileReader(file));
-        String st;
-        st = br.readLine();
-        while (!st.contains("package")) {
-            st = br.readLine();
-            if (st == null) {
-                return null;
+    private static List<Class<?>> loadClasses(Map<String,List<String>> classDefinitions){
+        List<Class<?>> classes = new ArrayList<>();
+        final Set<String> allDiscoveredClassNames = classDefinitions.keySet();
+        for(String discoveredClassName : allDiscoveredClassNames){
+            int numberOfClassesWithName = classDefinitions.get(discoveredClassName).size();
+            List<Class<?>> classesWithName = new ArrayList<>();
+            for(String singleClassLocation : classDefinitions.get(discoveredClassName)){
+                // Loop over all classes with a given class name (but not full name)
+                final String[] pathDirectories = singleClassLocation.split("/");
+                StringBuilder fullClassNameBuilder = new StringBuilder().append(discoveredClassName);
+                for(int i = pathDirectories.length - 2; i >= 0 ; i--){
+                    // -2 to ignore class name
+                    if(classesWithName.size() >= numberOfClassesWithName){
+                        break;
+                    }
+                    fullClassNameBuilder.insert(0,pathDirectories[i]+".");
+                    try {
+                        classesWithName.add(Class.forName(fullClassNameBuilder.toString()));
+                    } catch (ClassNotFoundException e){
+                        System.out.println(e.getStackTrace());
+                    }
+                }
             }
+            classes.addAll(classesWithName);
         }
-        String packageName = st.trim().substring(8, st.length() - 1);
-        String className = file.getName().substring(0, file.getName().length() - 5);
-        return (new StringBuilder()).append(packageName).append(".").append(className).toString();
+        return classes;
     }
 }
